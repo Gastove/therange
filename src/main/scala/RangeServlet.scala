540e1c45b0fb0cad305d54abc62dc3be21680b73
Ross Donaldson <ross@selfawaregames.com>
2013-08-10 20:26:41 -0700

    Adding making of, content tweaks.

--- src/main/scala/RangeServlet.scala
+++ src/main/scala/RangeServlet.scala
@@ -9,16 +9,26 @@ import collection.JavaConversions._
 
 class RangeServlet extends TheRangeStack {
 
+  val calaveraPath = "/WEB-INF/layouts/_calavera.jade"
+
   get("/") {
     contentType = "text/html"
     val avatarURL = "http://www.gravatar.com/avatar/" + 
       Md5.hash("gastove@gmail.com") +
       "?size=160"
 
-    jade("/home", "layout" -> "/WEB-INF/layouts/_calavera.jade", "avatarURL" -> avatarURL)
+    jade("/home", "layout" -> calaveraPath, "avatarURL" -> avatarURL)
 
   }
   
+  get("/themakingof") {
+    contentType = "text/html"
+    val avatarURL = "http://www.gravatar.com/avatar/" + 
+      Md5.hash("gastove@gmail.com") +
+      "?size=160"
+    jade("/makingOf", "layout" -> calaveraPath, "avatarURL" -> avatarURL)
+  }
+
   get("/twistory") {
     val tweetList = TweetSet.getHomeTimelineList
     val printable = tweetList.map{ status => status.getUser.getName + ":" + status.getText + "\n" }.toList
